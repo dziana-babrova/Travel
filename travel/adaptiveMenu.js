f48c39bfef4eb3432c7a3e1ea2fc215769e2236a
Diana Bobrova <dzianababrova@gmail.com>
2022-07-05 11:04:46 +0300

    feat: add adaptive menu

--- travel/adaptiveMenu.js
+++ travel/adaptiveMenu.js
@@ -3,32 +3,34 @@ let a = document.querySelector(".nav__showmenu");
 
     a.onclick = function() {
         let element = document.querySelector(".menu");
-        let shadow = document.querySelector(".wholepage")
+        let shadow = document.querySelector(".nav_back")
         element.classList.toggle("menu_active");
-        shadow.classList.toggle("shadow");
+        shadow.classList.toggle("nav_back_opacity");
 } // Menu is displayed upon clicking burger icon
 
 let b = document.querySelector(".cross");
 
     b.onclick = function () {
       let element = document.querySelector(".menu");
+      let shadow = document.querySelector(".nav_back");
       element.classList.toggle("menu_active");
-    }; // Menu is closed upon clicking cross icon
-
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    let currentScrollPos = window.pageYOffset;
-    let element = document.querySelector(".menu");
-  if (prevScrollpos < currentScrollPos) {
-    element.classList.remove("menu_active");
-  } 
-  
+      shadow.classList.toggle("nav_back_opacity");
+}; // Menu is closed upon clicking cross icon
+    
+let c = document.querySelector(".menu_list");
+    c.onclick = function () {
+      let element = document.querySelector(".menu");
+      let shadow = document.querySelector(".nav_back");
+      element.classList.remove("menu_active"); 
+      shadow.classList.remove("nav_back_opacity");
 }; // Menu is closed upon scrolling
 
 document.addEventListener("mousedown", function (e) {
   if (e.target.closest(".menu") === null) {
+    let shadow = document.querySelector(".nav_back");
     let element = document.querySelector(".menu");
     element.classList.remove("menu_active");
+    shadow.classList.remove("nav_back_opacity");
   }
 }); // Menu is closed upon tapping outside it
 
